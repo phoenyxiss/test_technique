c4f35e3827331559f220fa0bf1e4cdcfc5afc45d
Nix <phoenyxiss@laposte.net>
2022-11-28 02:11:11 +0100

    json return

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function response($json){
+        return response()->json($json);
+    }
 }
